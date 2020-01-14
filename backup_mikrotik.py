041aa274c7e9f32a1b4ee06f48f2122e39cd21bb
jorliano <jorliano@hotmail.com>
2020-01-13 21:25:35 -0300

    addition of scripts

---
+++ backup_mikrotik.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from netmiko import Netmiko
+import os
+
+listaips = "C:/Users/leand/Documents/Redes/listaips/ips.txt"
+dir_backup = "C:/Users/leand/Documents/Redes/backup"
+
+with open(listaips) as ips:
+   for cnt, ip in enumerate(ips):
+        print(ip)
+
+        try:
+            #dados da conexão do equipamento
+            plataforma = {
+                "host": ip,
+                "username": "admin",
+                "password": "admin",
+                "device_type": "mikrotik_routeros",
+            }
+
+            #conexão e comandos via ssh
+            net_connect = Netmiko(**plataforma)
+            print(net_connect.find_prompt())
+            output = net_connect.send_command("export")
+            hostname = net_connect.send_command("/system identity print")
+            net_connect.disconnect()
+
+            #definindo diretorio dos backups
+            os.chdir(dir_backup)
+            hostname = hostname.strip().replace('name: ', '').lower()
+
+            #criando arquivo de backup
+            filen= open(hostname+".cfg", 'w')
+            filen.write(output)
+            filen.close()
+
+            print("Backup realizado com sucesso")
+        except Exception as e:
+            print("Ouve um erro :")
+            print(e)
