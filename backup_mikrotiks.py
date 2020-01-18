2b1a2995a0b89c14ef776dc0561a34a83b088215
jorliano <jorliano@hotmail.com>
2020-01-18 19:16:28 -0300

    melhorando codigo

--- backup_mikrotiks.py
+++ backup_mikrotiks.py
@@ -11,38 +11,40 @@ if not os.path.exists(path):
     os.mkdir(path)
     os.mkdir(dir_backup)
     file_ips = open(path_ips, 'w')
+    file_ips.write("#Digite seus ips de bakup a baixo, caso queria comentar adicione '#' na frente\n")
     file_ips.write("192.168.0.88")
     file_ips.close()
 
 
 with open(path_ips) as ips:
    for cnt, ip in enumerate(ips):
-        print(ip)
+       if ip[0] != "#":
+         print(ip)
 
-        try:
-            #dados da conexao do equipamento
-            plataforma = {
-                "host": ip,
-                "username": "admin",
-                "password": "admin",
-                "device_type": "mikrotik_routeros",
-            }
+         try:
+             #dados da conexao do equipamento
+             plataforma = {
+                 "host": ip,
+                 "username": "admin",
+                 "password": "admin",
+                 "device_type": "mikrotik_routeros",
+             }
 
-            #conexao e comandos via ssh
-            net_connect = Netmiko(**plataforma)
-            print(net_connect.find_prompt())
-            output = net_connect.send_command("export")
-            hostname = net_connect.send_command("/system identity print")
-            net_connect.disconnect()
+             #conexao e comandos via ssh
+             net_connect = Netmiko(**plataforma)
+             print(net_connect.find_prompt())
+             output = net_connect.send_command("export")
+             hostname = net_connect.send_command("/system identity print")
+             net_connect.disconnect()
 
-            hostname = hostname.strip().replace('name: ', '').lower()
+             hostname = hostname.strip().replace('name: ', '').lower()
 
-            #criando arquivo de backup
-            filen= open(dir_backup+"/"+hostname+".rsc", 'w')
-            filen.write(output)
-            filen.close()
+             #criando arquivo de backup
+             filen= open(dir_backup+"/"+hostname+".rsc", 'w')
+             filen.write(output)
+             filen.close()
 
-            print("Backup realizado com sucesso")
-        except Exception as e:
-            print("Ouve um erro :")
-            print(e)
+             print("Backup realizado com sucesso")
+         except Exception as e:
+             print("Ouve um erro :")
+             print(e)
--- backup_radios.py
+++ backup_radios.py
@@ -11,38 +11,40 @@ if not os.path.exists(path):
     os.mkdir(path)
     os.mkdir(dir_backup)
     file_ips = open(path_ips, 'w')
+    file_ips.write("#Digite seus ips de bakup a baixo, caso queria comentar adicione '#' na frente\n")
     file_ips.write("192.168.1.20")
     file_ips.close()
 
 
 with open(path_ips) as ips:
    for cnt, ip in enumerate(ips):
-        print(ip)
+      if ip[0] != "#":
+          print(ip)
 
-        try:
-            #dados da conexao do equipamento
-            plataforma = {
-                "host": ip,
-                "username": "admin",
-                "password": "admin",
-                "device_type": "linux",
-            }
+          try:
+              #dados da conexao do equipamento
+              plataforma = {
+                  "host": ip,
+                  "username": "admin",
+                  "password": "admin",
+                  "device_type": "linux",
+              }
 
-            #conexao e comandos via ssh
-            net_connect = Netmiko(**plataforma)
-            print(net_connect.find_prompt())
-            output = net_connect.send_command("cat /tmp/system.cfg")
-            hostname = net_connect.send_command("cat /proc/sys/kernel/hostname")
-            net_connect.disconnect()
+              #conexao e comandos via ssh
+              net_connect = Netmiko(**plataforma)
+              print(net_connect.find_prompt())
+              output = net_connect.send_command("cat /tmp/system.cfg")
+              hostname = net_connect.send_command("cat /proc/sys/kernel/hostname")
+              net_connect.disconnect()
 
-            hostname = hostname.strip().lower()
+              hostname = hostname.strip().lower()
 
-            #criando arquivo de backup
-            filen= open(dir_backup+"/"+hostname+".cfg", 'w')
-            filen.write(output)
-            filen.close()
+              #criando arquivo de backup
+              filen= open(dir_backup+"/"+hostname+".cfg", 'w')
+              filen.write(output)
+              filen.close()
 
-            print("Backup realizado com sucesso")
-        except Exception as e:
-            print("Ouve um erro :")
-            print(e)
+              print("Backup realizado com sucesso")
+          except Exception as e:
+              print("Ouve um erro :")
+              print(e)
